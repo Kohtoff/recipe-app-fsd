3b408288896b0171d579266aae8abbdaa146ca40
Kohtoff <danil.kohtoff@gmail.com>
2025-03-14 16:14:45 +0100

    define base styles theme

--- src/app/routers/AppRouter.tsx
+++ src/app/routers/AppRouter.tsx
@@ -1,16 +1,30 @@
-import { createRoutesFromElements, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import { RootLayout } from "app/layout/root";
 import { Fallback } from "shared/ui/fallback";
 import { HomePage } from "pages/home";
-
+import { Recipe } from "pages/recipe";
 
 export const AppRouter = () => {
   const routers = createRoutesFromElements(
-    <Route path="/" element={<RootLayout />} errorElement={<Fallback />}>
-      <Route index element={<HomePage />} />
-    </Route>
+    <>
+      <Route path="/" element={<RootLayout />} errorElement={<Fallback />}>
+        <Route index element={<HomePage />} />
+      </Route>
+      <Route path="/recipe" element={<RootLayout />}>
+        <Route path=":id" index element={<Recipe />} />
+      </Route>
+
+      <Route path="*" element={<Fallback />} />
+    </>
   );
-  return <div>AppRouter</div>;
+
+  const router = createBrowserRouter(routers, {});
+  return <RouterProvider router={router} />;
 };
 
 export default AppRouter;
